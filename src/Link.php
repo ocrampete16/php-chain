14930ca4313e1e41829a24730c156707047f3031
Marco Petersen <m@rcopetersen.com>
2017-03-03 00:06:54 +0100

    Make Link chainable

--- src/Link.php
+++ src/Link.php
@@ -5,13 +5,34 @@ namespace MarcoPetersen\Chain;
 abstract class Link
 {
     /**
+     * @var Link
+     */
+    private $successor;
+
+    /**
+     * @param Link $successor
+     *
+     * @return $this
+     */
+    public function then(Link $successor)
+    {
+        $this->successor = $successor;
+
+        return $this;
+    }
+
+    /**
      * @param $payload
      *
      * @return mixed
      */
     protected function next($payload)
     {
-        return $payload;
+        if (!$this->successor) {
+            return $payload;
+        }
+
+        return $this->successor->execute($payload);
     }
 
     /**
--- tests/LinkTest.php
+++ tests/LinkTest.php
@@ -21,4 +21,10 @@ class LinkTest extends TestCase
     {
         $this->assertEquals(2, (new AddOne())->execute(1));
     }
+
+    /** @test */
+    public function chain_to_another_link()
+    {
+        $this->assertEquals(3, (new AddOne())->then(new AddOne())->execute(1));
+    }
 }
